dd2cb9833a86b9f62f921fe8dc0dd8583ede583b
Lauren Stone <lstone321@gmail.com>
2020-08-12 03:10:16 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,9 +8,9 @@ class Backer
     @backed_projects = []
   end 
   
-  def back_project(project) 
+ def back_project(project)
     @backed_projects << project
-    
+    project.backers << self
   end 
   
 end 
