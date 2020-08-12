c483e6eb3b90db41cc3ff1af4af318e2cb22756b
Lauren Stone <lstone321@gmail.com>
2020-08-12 03:12:59 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,16 +1,13 @@
 class Project
-  
-  attr_reader :title
-  attr_accessor :backers 
-  
+  attr_accessor :title, :backers
+
   def initialize(title)
     @title = title
     @backers = []
-  end 
-  
-   def add_backer(backer)
-    @backers << backer
-    backer.back_project << self
   end
-  
-end 
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+end
